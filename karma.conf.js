3d57d45c3efca42a1900805fcc54591756a5a51a
Benjamin Goering <bengoering@gmail.com>
2014-03-31 05:33:11 -0700

    use mocha-debug with karma

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
 
 
     // frameworks to use
-    frameworks: ['mocha', 'cajon'],
+    frameworks: ['mocha-debug', 'mocha', 'cajon'],
 
 
     // list of files / patterns to load in the browser
