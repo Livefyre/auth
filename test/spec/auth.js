c11bd1472eed583b473fb33395c0f4bc74b724ee
Benjamin Goering <bengoering@gmail.com>
2014-03-30 17:53:57 -0700

    Correct test for spec/auth

--- test/spec/auth.js
+++ test/spec/auth.js
@@ -1,8 +1,8 @@
-var createAuth = require('auth/auth');
+var Auth = require('auth/auth');
 var assert = require('chai').assert;
 
 describe('auth/auth', function () {
-    it('exports a factory function', function () {
-        assert(true);
+    it('exports a constructor function', function () {
+        assert.typeOf(Auth, 'function');
     });
 });
